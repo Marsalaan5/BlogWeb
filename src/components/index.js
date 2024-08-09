3cdaf38014a36b8256d4a733ba9d672b359fdf63
Marsalaan5 <arsalaankhan5@gmail.com>
2024-08-09 16:19:02 +0530

    edited

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,11 @@
-
 import Select from "./Select";
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Contact from "./Contact";
 import Home from "./Home/Home";
-
 import Container from "./Container/Container";
-import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
@@ -20,22 +17,20 @@ import AuthLayout from "./AuthLayout";
 import Input from "./Input";
 
 export {
-    Home,
-    Carousel,
-    Header,
-    Footer,
-    Contact,
-    Container,
-    Logo,
-    LogoutBtn,
-    RTE,
-    Signup,
-    Login,
-    Button,
-    PostForm,
-    PostCard,
-    AuthLayout, 
-    Input,
-    Select,
-   
-}
+  Home,
+  Carousel,
+  Header,
+  Footer,
+  Contact,
+  Container,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
